deb52d0795dc7318eec507c7196be1b06aef2c5c
elpilototec <cesarnicolasogando1@gmail.com>
2024-10-04 22:47:14 +0200

    Refactorizar URL de producción en config.js

--- src/config.js
+++ src/config.js
@@ -1,5 +1,5 @@
 const API_URL = process.env.NODE_ENV === 'production' 
-  ? 'https://www.trendtubeai.com/api'  // URL para producción
+  ? 'https://www.trendtubeai.com'  // URL para producción
   : 'http://localhost:3001';           // URL para desarrollo
 
 export default API_URL;
